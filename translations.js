4469aeab165472d6022797baae7c948f392ce2b5
AlbertGalic <albertgalic@gmail.com>
2025-03-29 18:48:21 +0100

    Dodani prijevodi i multijezični sprint intro

---
+++ translations.js
@@ -0,0 +1,17 @@
+const translations = {
+  en: {
+    title: "Sprint Test",
+    description: "You will perform 3 sprints with 5 minutes rest between.",
+    button: "Continue"
+  },
+  hr: {
+    title: "Sprint Test",
+    description: "Izvest ćete 3 sprinta s 5 minuta odmora između.",
+    button: "Nastavi"
+  },
+  de: {
+    title: "Sprint-Test",
+    description: "Du führst 3 Sprints mit jeweils 5 Minuten Pause durch.",
+    button: "Weiter"
+  }
+};
